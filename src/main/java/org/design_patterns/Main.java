b84f774b47f1504e42feee1a1b30ebac8e1ba473
mahmoudmatar <mahmoudmatar49@gmail.com>
2024-11-12 18:53:49 +0200

    add welcome message

---
+++ src/main/java/org/design_patterns/Main.java
@@ -0,0 +1,7 @@
+package org.design_patterns;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to our design patterns course revision");
+    }
+}
